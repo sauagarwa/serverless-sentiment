c285c9a3af918603df99839a4772a2e5a5e4acc2
Saurabh Agarwal <sauagarw@redhat.com>
2024-01-18 17:05:23 -0500

    Update frontend-app.component.ts

--- frontend/src/app/frontend-app/frontend-app.component.ts
+++ frontend/src/app/frontend-app/frontend-app.component.ts
@@ -19,7 +19,7 @@ export class FrontendAppComponent {
     this.apiText='';
     this.apiSentimentNum='';
     // Simple POST request with a JSON body and response type <any>
-    this.http.post<any>('https://sentiment-sentiment-project.apps.rosa-7xzvq.cynv.p1.openshiftapps.com'+'/api/post_sentiment', { text: this.Sentence.value }).subscribe(data => {
+    this.http.post<any>('https://sentiment-sentiment-project.apps.ai-dev01.kni.syseng.devcluster.openshift.com'+'/api/post_sentiment', { text: this.Sentence.value }).subscribe(data => {
       this.apiSentimentNum = data.sentiment;
       this.apiText = data.text;
     // the following code specifies the range of each sentiment which its value is min=-1 and max=1, can be customized
